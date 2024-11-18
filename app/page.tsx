2ba23e08e7030298808f710cc3de06a28e4bb19e
nuller <rafaelcassandras@gmail.com>
2024-11-18 15:43:37 +0000

    18-11-24

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="center">
+    <div className="flex-center">
       <h1 className="h1">Nexus - The Next Cloud for all of us.</h1>
     </div>
   );
